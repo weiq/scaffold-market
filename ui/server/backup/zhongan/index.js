87b09684ea6cf0b54bc2adb795909af3bc4f0d92
Travis CI <surgesoft@gmail.com>
2017-06-19 13:26:34 +0000

    Deploy to GitHub Pages: 2b120d64708a8bc9298ce4a53d2ed04502d28305

--- ui/server/backup/zhongan/index.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by tianzx on 2016/11/29.
- */
--- ui/server/backup/zhongan/utils.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by tianzx on 2016/11/29.
- */
-export const ZHONGAN_POLICE = "/zhongan/policy";
-export const ZHONGAN_REBATE = "/zhongan/rebate";
